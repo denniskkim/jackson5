25e5d2ded64a0acd3824dcaf9b49881b56159e8f
Wingo Zhai <zizhou.zhai@hotmail.co.nz>
2016-02-17 16:44:02 -0800

    fixed landing page path

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,8 @@
  * Home page.
  */
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+  //res.render('home', {
+  //  title: 'Home'
+  //});
+  res.render('landing_page');
 };
