e00e313b7f95f0057af2e12894116fee3e9b1582
Wingo Zhai <zizhou.zhai@hotmail.co.nz>
2016-03-01 17:11:20 -0800

    fixed subdoamin bug

--- controllers/home.js
+++ controllers/home.js
@@ -7,7 +7,7 @@ exports.index = function(req, res) {
   var domain = req.headers.host,
       subDomain = domain.split('.');
 
-  if(subDomain.length > 1){
+  if(subDomain.length > 2){
     subDomain = subDomain[0].split("-").join(" ");
     if (req.user) {
       return res.redirect('/dashboard_employee');
