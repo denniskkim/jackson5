11ddfc1582211c6fd1a886b988abbe6f371f2e57
Wingo Zhai <zizhou.zhai@hotmail.co.nz>
2016-03-02 03:48:43 -0800

    testing what would happen if removing the hacky subdomain

--- controllers/home.js
+++ controllers/home.js
@@ -12,29 +12,29 @@ exports.index = function(req, res) {
   var domain = req.headers.host,
       subDomain = domain.split('.');
 
-  if(subDomain.length > 2){
-  //User.findOne({ subdomainurl: subDomain }, function(err, user) {
-  //  booleanHolder = 1;
-  //});
-
-  //if(booleanHolder == 1){
-    subDomain = subDomain[0].split("-").join(" ");
-    if (req.user) {
-      return res.redirect('/dashboard_employee');
-    }
-    else{
-      res.redirect('/login_employee');
-    }
-
-  }
-  else{
+  //if(subDomain.length > 2){
+  ////User.findOne({ subdomainurl: subDomain }, function(err, user) {
+  ////  booleanHolder = 1;
+  ////});
+  //
+  ////if(booleanHolder == 1){
+  //  subDomain = subDomain[0].split("-").join(" ");
+  //  if (req.user) {
+  //    return res.redirect('/dashboard_employee');
+  //  }
+  //  else{
+  //    res.redirect('/login_employee');
+  //  }
+  //
+  //}
+  //else{
     if (req.user) {
       return res.redirect('/dashboard_admin');
     }
     else{
       res.render('home');
     }
-  }
+  //}
 
 
 
